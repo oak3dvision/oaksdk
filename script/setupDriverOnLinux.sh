d6e48bd06a7fa4d08f620c7bc1cde74591250b67
Alonzo <Alonzo@qq.com>
2020-06-23 10:54:14 +0800

    Update driver install script on linux

--- script/setupDriverOnLinux.sh
+++ script/setupDriverOnLinux.sh
@@ -1,5 +1,7 @@
 sudo cp ./script/88-OakCDK.rules /etc/udev/rules.d/
+sudo cp ./script/89-OakQCDK.rules /etc/udev/rules.d/
 
 sudo chmod a+x /etc/udev/rules.d/88-OakCDK.rules
+sudo chmod a+x /etc/udev/rules.d/89-OakQCDK.rules
 
 sudo udevadm control --reload
